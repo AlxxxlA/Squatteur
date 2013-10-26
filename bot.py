81b88d05f7e6fbd6e0dddbf1c3b8fbf7fabc132e
Alx <alex@jauneau.eu>
2013-10-27 01:47:03 +0200

    some fixe

--- bot.py
+++ bot.py
@@ -33,13 +33,15 @@ class Bot(ircbot.SingleServerIRCBot):
     def on_join(self, serv, ev):
         serv.privmsg(ev.target(), "Coucou ! Tu veux voir mon boulon ?")
 
+
     def on_pubmsg(self, serv, ev):
         """
         Réception des messages du chan
         """
         auteur = irclib.nm_to_n(ev.source())
         canal = ev.target()
-	msg = ev.arguments()[0]
+        users = self.channels[canal].users()
+        msg = ev.arguments()[0]
         message = msg.lower()
         messages = message.split(' ')
         
@@ -50,7 +52,7 @@ class Bot(ircbot.SingleServerIRCBot):
         if 'lol' in message:
             serv.privmsg(canal, "On ne lol pas ici.")
 # Roulette russe
-        if message.lower() == "!roll":
+        if message.lower() == conf['command']['roll']:
             
             random = randrange(0, self.chambre)
             clic = randrange(0, self.chambre)
@@ -64,23 +66,25 @@ class Bot(ircbot.SingleServerIRCBot):
                 self.chambre = self.chambre - 1
 
 # Partager un lien sur shaarly                
-        if messages[0] == "!share":
+        if messages[0] == conf['command']['shaarli']:
             if len(messages) > 1:
                 url = messages[1]
-                get_params = {'token_auth' : conf['shaarly']['token'], 'post' : url}
+                get_params = {'token_auth' : conf['shaarli']['token'], 'post' : url}
                 if len(messages) == 3:
                     get_params["tags"] = messages[2].replace(',', ' ')
                 if len (messages) == 4:
                     get_params["description"] = message[4]
-                print urllib.urlopen(conf['shaarly']['host']+urllib.urlencode(get_params))
+                print urllib.urlopen(conf['shaarli']['host']+urllib.urlencode(get_params))
 
 
 ## Passer tous les participants en opérateur du chan
-        if message.lower() == "!op":
-            serv.mode(canal,"+o Grizby")
+        if messages[0] == conf['command']['op']:
+            print(users)
+            print("+o"*len(users)+' '.join(users))
+            serv.mode(canal, "+"+"o"*len(users)+' '+' '.join(users))
             print(message)
 
-        if messages[0] == "!help":
+        if messages[0] == conf['command']['help']:
             if len(messages) > 1:
                 command = messages[1]
                 for command in help.keys():
@@ -88,7 +92,7 @@ class Bot(ircbot.SingleServerIRCBot):
                         serv.privmsg(canal, help[command].encode('utf-8'))
             else:
                 serv.privmsg(canal, "Liste des commandes disponibles")
-                serv.privmsg(canal, help.keys())
+                serv.privmsg(canal, conf['command'].keys())
             print(messages)
 
             
